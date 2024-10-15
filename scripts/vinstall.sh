52f7f497a3aff725c17b876e1904e78b31313d97
ziqiao zhou <ziqiaozhou@microsoft.com>
2024-10-15 23:47:55 +0000

    upgrade cargo-v and verus-rustc

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-cargo install --git https://github.com/microsoft/verismo/ --rev 1114995 cargo-v
+VERISMO_REV=6881a3b
+cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV cargo-v
 builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin_macros") | .targets[].src_path'`
 verus=`dirname $builtin`/../../../source/target-verus/release/verus
 if [ -f ${verus} ]; then
@@ -7,5 +8,5 @@ if [ -f ${verus} ]; then
 else
     cargo v prepare-verus 
 fi
-cargo install --git https://github.com/microsoft/verismo/ --rev 1114995 verus-rustc
+cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV verus-rustc
 curl --proto '=https' --tlsv1.2 -LsSf https://github.com/verus-lang/verusfmt/releases/download/v0.4.3/verusfmt-installer.sh | sh
