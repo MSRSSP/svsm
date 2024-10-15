e41f4fb42e4312f7ecbf014f4104b866c0effeeb
ziqiao zhou <ziqiaozhou@microsoft.com>
2024-10-15 23:26:51 +0000

    fix error in scripts due to builtin not found

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cargo install --git https://github.com/microsoft/verismo/ --rev 1114995 cargo-v
-builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin") | .targets[].src_path'`
+builtin=`cargo metadata --format-version 1 | jq -r '.packages[] | select(.name == "builtin_macros") | .targets[].src_path'`
 verus=`dirname $builtin`/../../../source/target-verus/release/verus
 if [ -f ${verus} ]; then
     echo "verus (${verus}) is already built"
