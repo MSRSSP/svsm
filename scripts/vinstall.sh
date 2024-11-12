9459b7feb127c20d7b2b7781656a1cf02b3e571a
Ziqiao Zhou <ziqiaozhou@microsoft.com>
2024-11-12 13:19:58 -0800

    Install verusfmt via cargo install

--- scripts/vinstall.sh
+++ scripts/vinstall.sh
@@ -6,8 +6,8 @@ verus=`dirname $builtin`/../../../source/target-verus/release/verus
 if [ -f ${verus} ]; then
     echo "verus (${verus}) is already built"
 else
-    cargo v prepare-verus 
+    cargo v prepare-verus
 fi
 cargo install --git https://github.com/microsoft/verismo/ --rev $VERISMO_REV verus-rustc
-curl --proto '=https' --tlsv1.2 -LsSf https://github.com/verus-lang/verusfmt/releases/download/v0.5.0/verusfmt-installer.sh | sh
-sudo apt-get install build-essential ninja-build libclang-dev
+## Install verus formatter
+cargo install --git https://github.com/verus-lang/verusfmt --rev v0.5.0
